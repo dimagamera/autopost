2648e7a3bc5357c4a60745ea7da49f786c3eb8d6
dimagamera <note.dima.gamera@gmail.com>
2020-05-01 16:53:06 +0400

    Update

--- autopost/start.py
+++ autopost/start.py
@@ -2,12 +2,12 @@ from telethon import functions, types
 from telethon.sync import TelegramClient
 from telethon import TelegramClient, events, sync
 from settings import api_id, api_hash
-
+#
 client = TelegramClient("test", api_id, api_hash)
 client.start()
 print("STARTED")
 
-@client.on(events.NewMessage(chats=["https://t.me/joinchat/AAAAAEY0fM9SbvX_gEEpKg", "https://t.me/joinchat/AAAAAE6Evnojp0kGxAltxQ", "http://t.me/joinchat/AAAAAFIGEJVBjoskE_TvuA", "https://t.me/COVID19_Ukraine", "https://t.me/joinchat/AAAAAEn9-GcgeevVb3Urtg"]))
+@client.on(events.NewMessage(chats=["https://t.me/joinchat/AAAAAEY0fM9SbvX_gEEpKg", "https://t.me/joinchat/AAAAAE6Evnojp0kGxAltxQ", "http://t.me/joinchat/AAAAAFIGEJVBjoskE_TvuA", "https://t.me/COVID19_Ukraine"]))
 async def normal_handler(event):
     if isinstance(event.chat, types.Channel):
         username = event.chat.username
